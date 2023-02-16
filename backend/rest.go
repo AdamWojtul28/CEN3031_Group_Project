b3466547b602021dcb3c6457ed5219ca937772df
Adam Wojtulewski <adamwojtulewski@ufl.edu>
2023-02-16 17:28:33 -0500

    Darrion's changes to the backend

---
+++ backend/rest.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+// httpHandler creates the backend HTTP router for queries, types,
+// and serving the Angular frontend.
+func httpHandler() http.Handler {
+	router := mux.NewRouter().StrictSlash(true)
+	// Your REST API requests go here
+
+	router.HandleFunc("/api/users", GetUsers).Methods("GET")
+	router.HandleFunc("/api/users/{id}", GetUserById).Methods("GET")
+	router.HandleFunc("/api/users", CreateUser).Methods("POST")
+	router.HandleFunc("/api/users/{id}", UpdateUser).Methods("PUT")
+	router.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
+	// Add your routes here.
+
+	// WARNING: this route must be the last route defined.
+	router.PathPrefix("/").Handler(AngularHandler).Methods("GET")
+
+	/**
+	 * We need some headers to be statically prepended to every response.
+	 */
+	return handlers.LoggingHandler(os.Stdout,
+		handlers.CORS(
+			handlers.AllowCredentials(),
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization",
+				"DNT", "Keep-Alive", "User-Agent", "X-Requested-With", "If-Modified-Since",
+				"Cache-Control", "Content-Range", "Range"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"http://localhost:8080"}),
+			handlers.ExposedHeaders([]string{"DNT", "Keep-Alive", "User-Agent",
+				"X-Requested-With", "If-Modified-Since", "Cache-Control",
+				"Content-Type", "Content-Range", "Range", "Content-Disposition"}),
+			handlers.MaxAge(86400),
+		)(router))
+}
