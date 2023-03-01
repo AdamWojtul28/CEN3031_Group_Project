2f5a9414a919d104ad9febb1db816fc717aaf3e0
Alex Vargas <alex48vargas@gmail.com>
2023-03-01 14:41:05 -0500

    added cypress testing e2e

--- webapp-test/cypress/e2e/spec.cy.ts
+++ webapp-test/cypress/e2e/spec.cy.ts
@@ -1,9 +1,15 @@
-describe('My First Test', () => {
-  it('Find Create User', () => {
+describe('Users Test', () => {
+  it('Creates user123', () => {
     cy.visit('http://localhost:5000/')
     cy.get('[name="username"]').type('user123')
     cy.get('[name="password"]').type('pass123')
     cy.contains('Create User').click()
     cy.contains('Fetch Users').click()
   })
+
+  // it('Deletes user123' , () => {
+  //   cy.visit('http://localhost:5000/')
+  //   cy.get('[name="username"]').type('user123')
+  // })
+
 })
