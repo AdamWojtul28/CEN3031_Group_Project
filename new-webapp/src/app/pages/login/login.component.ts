161c3fca1023de725279ea744dd70d05e3c9278b
Alex Vargas <alex48vargas@gmail.com>
2023-03-01 18:03:13 -0500

    login page form/http

--- new-webapp/src/app/pages/login/login.component.ts
+++ new-webapp/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm }  from '@angular/forms';
+
+import { User } from 'src/app/models/user.model';
+import { UsersHttpService } from 'src/app/services/users-http.service';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  @ViewChild('f') loginForm!: NgForm;
+
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
-  constructor(){}
+
+  userInfo: User = {
+    username: '',
+    password: '',
+    email: '',
+    bio: ''
+  }
+
+  constructor(private userHttp: UsersHttpService){}
   
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"
     this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSubmitLogin(){
+    console.log(this.loginForm);
+    this.userInfo.username = this.loginForm.value.username;
+    this.userInfo.password = this.loginForm.value.password;
+    this.userHttp.loginUser(this.userInfo.username, this.userInfo.password).subscribe();
   }
 }
---
+++ new-webapp/src/app/services/users-http.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { User } from "../models/user.model";
+
+import { map } from 'rxjs/operators';
+
+//https://worldier-testing-default-rtdb.firebaseio.com/users.
+
+@Injectable({providedIn: 'root'})
+export class UsersHttpService {
+  constructor(private http: HttpClient) {}
+
+  createNewUser(username: string, password: string, email: string) {
+    const user: User = {username: username, password: password, email: email, bio: ''};
+    return this.http.post('http://localhost:5000/api/users', user);
+  }
+
+  loginUser(username: string, password: string) {
+    const user: User = {username: username, password: password, email: '', bio: ''};
+    return this.http.post('http://localhost:5000/api/users', user);
+  }
+
+  deleteUser(userId: number) {
+    return this.http.delete('http://localhost:5000/api/users/' + userId);
+  }
+
+  fetchUsers() {
+    return this.http
+      .get<{[key: number] : User}>('http://localhost:5000/api/users')
+        .pipe(
+          map(data => {
+            const usersArray : User[] = [];
+            for (const key in data) {
+              usersArray.push(data[key]);
+            }
+            return usersArray;
+          })
+        )
+  }
+}
