121938531c3c88e0e12f5c6e47f53021ac44f50a
Darrion <ramosdarrion@ufl.edu>
2023-03-29 16:43:25 -0400

    updated SQL files

--- backend/SQL_Files/worldlier_data_users.sql
+++ backend/SQL_Files/worldlier_data_users.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: localhost    Database: worldlier
+-- Host: localhost    Database: crud_user
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -14,6 +14,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `connections`
+--
+
+DROP TABLE IF EXISTS `connections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `connections` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `sender` longtext NOT NULL,
+  `reciever` longtext NOT NULL,
+  `status` longtext NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `idconnections_UNIQUE` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connections`
+--
+
+LOCK TABLES `connections` WRITE;
+/*!40000 ALTER TABLE `connections` DISABLE KEYS */;
+INSERT INTO `connections` VALUES (1,'John3','Bob','Pending');
+/*!40000 ALTER TABLE `connections` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `listings`
@@ -101,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-29 16:40:07
+-- Dump completed on 2023-03-29 16:43:11
