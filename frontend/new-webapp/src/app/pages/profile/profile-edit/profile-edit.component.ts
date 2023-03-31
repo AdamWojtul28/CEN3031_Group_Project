bc0668d3883732ecc0104f22619cc980e9dc76bf
Alex Vargas <alex48vargas@gmail.com>
2023-03-31 15:40:42 -0400

    edit profile wireframe

---
+++ frontend/new-webapp/src/app/pages/profile/profile-edit/profile-edit.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UsersHttpService } from 'src/app/services/users-http.service';
+
+@Component({
+  selector: 'app-profile-edit',
+  templateUrl: './profile-edit.component.html',
+  styleUrls: ['./profile-edit.component.css']
+})
+export class ProfileEditComponent implements OnInit{
+  user: User;
+  section: string = 'details'
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.navigate(['profile/details']);
+  }
+
+  onChooseSection(section: string) {
+    this.section = section;
+  }
+}
