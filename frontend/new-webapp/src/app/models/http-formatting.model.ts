1e7b3b9765f9cb8fc065e2b6cb39d2ec78ca4bef
Alex Vargas <alex48vargas@gmail.com>
2023-04-06 15:44:53 -0400

    profile update changes

--- frontend/new-webapp/src/app/models/http-formatting.model.ts
+++ frontend/new-webapp/src/app/models/http-formatting.model.ts
@@ -3,8 +3,5 @@ export interface UpdateProfileInfoModel{
   username? : string;
   email? : string;
   password? : string;
-}
-
-export interface UpdatePfpModel {
-  image : string | ArrayBuffer;
+  profile_image? : string | ArrayBuffer;
 }
