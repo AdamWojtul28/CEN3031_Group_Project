be5b1941877f8429693559ad78b2a46dd52070c8
NicoVLo <nicolasvlopera@gmail.com>
2023-04-19 11:00:00 -0400

    -Home page update

--- frontend/new-webapp/src/app/pages/home/home.component.ts
+++ frontend/new-webapp/src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  title = 'your-app-name';
 
+  ngAfterViewInit() {
+    const options = {
+      strings: ['the United States', 'Poland', 'the United Kingdom'],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true,
+      
+    };
+
+    const typed = new Typed('.auto-type', options);
+  }
 }
